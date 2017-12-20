469482eaa9ef292f9017c4e29495310429fcd91d
mars485 <ainur.karpiev@yandex.ru>
2017-12-20 20:45:54 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php $var='Пример переменной';?>
+<?php $title='Уроки по PHP';?>
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title;?></title>
+	<link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
+	<?php
+        echo '<h1>Привет Мир!!!</h1>';
+        print '<p>Привет Мир!!!</p>';
+        echo "<h2>$var<h2>";
+    ?>
+</body>
+</html>
